35b4865280126b756299093b62699adae124fd9d
Ayushi <ayushishrivastava2005@gmail.com>
2026-09-03 21:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (87.77%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,38 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-
-        for (int targetParity = 0; targetParity <= 1; targetParity++) {
-            boolean possible = true;
-
-            for (int i = 0; i < n; i++) {
-                boolean canForm = false;
-
-                if (Math.abs(nums1[i]) % 2 == targetParity) {
-                    canForm = true;
-                } else {
-                    for (int j = 0; j < n; j++) {
-                        if (i != j) {
-                            int diff = nums1[i] - nums1[j];
-                            if (Math.abs(diff) % 2 == targetParity) {
-                                canForm = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                if (!canForm) {
-                    possible = false;
-                    break;
-                }
-            }
-
-            if (possible) {
-                return true;
-            }
-        }
-
-        return false;
+        return true;
     }
 }
